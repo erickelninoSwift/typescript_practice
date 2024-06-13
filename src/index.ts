b887b3f071b6c4ac495073891d260559dcf5b45b
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-13 23:31:18 +0200

    done with all typescript setup and mongo db

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import http from "http";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-const PORT = 8080;
+
+const PORT = process.env.PORT;
 app.use(
   cors({
     credentials: true,
@@ -18,8 +22,16 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
+const server: http.Server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log("server is running very well ");
+  console.log("Connected to server on PORT : ", PORT);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.URI).then(() => {
+  console.log("Connected to DB ");
+});
+mongoose.connection.on("erro", (error: Error) => {
+  console.log(`Error found while connecting to mongoDB ${error}`);
 });
